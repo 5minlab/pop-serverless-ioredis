beada7724596a9f66968c893328954aea990970f
if1live <libsora25@gmail.com>
2019-09-01 14:33:32 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"makeMockRedis","url":"globals.html#makemockredis","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"patchRedis","url":"globals.html#patchredis","classes":"tsd-kind-function tsd-is-not-exported"},{"id":2,"kind":64,"name":"make_zrevrank","url":"globals.html#make_zrevrank","classes":"tsd-kind-function tsd-is-not-exported"},{"id":3,"kind":64,"name":"make_zrank","url":"globals.html#make_zrank","classes":"tsd-kind-function tsd-is-not-exported"}]};
